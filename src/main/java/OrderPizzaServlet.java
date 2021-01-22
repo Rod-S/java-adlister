45fa88ef1a3a55347fa528b61d40ef0ba5f7c080
Rod-S <rodsanati@gmail.com>
2021-01-21 21:21:12 -0600

    complete orderpizza

--- src/main/java/OrderPizzaServlet.java
+++ src/main/java/OrderPizzaServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 @WebServlet(name="OrderPizzaServlet", urlPatterns="/orderpizza")
 public class OrderPizzaServlet extends HttpServlet {
@@ -11,7 +12,18 @@ public class OrderPizzaServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         req.getRequestDispatcher("/pizza-order/orderpizza.jsp").forward(req, res);
     }
-    protected void doPost(HttpServletRequest req, HttpServletResponse res) {
-
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        for(String name : req.getParameterMap().keySet()) {
+            for (String value : req.getParameterValues(name)) {
+                if (name.equalsIgnoreCase("toppings")) {
+                    String[] toppings = req.getParameterValues("toppings");
+                    req.setAttribute("toppings", toppings);
+                } else {
+                req.setAttribute(name, value);
+                }
+                System.out.println(name + ": " + value);
+            }
+        }
+        req.getRequestDispatcher("/pizza-order/orderpizza.jsp").forward(req, res);
     }
 }
--- src/main/java/PickColorServlet.java
+++ src/main/java/PickColorServlet.java
@@ -10,12 +10,4 @@ public class PickColorServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         req.getRequestDispatcher("/color-app/pickcolor.jsp").forward(req, res);
     }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String crust
-                String sauce
-                        String toppings
-                                String address = req.getParameter("")
-    }
 }
